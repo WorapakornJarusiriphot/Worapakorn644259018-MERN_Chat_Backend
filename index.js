5d44aa3370115cfa421a224cc451b64a590c2ece
worapakorn <pupupuwora@gmail.com>
2024-01-20 18:59:19 +0700

    Add File Message.js in Folder models || Update index.js HAVE const Message = require("./models/Message"); --const ws = require("ws"); --const fs = require("fs");

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const User = require("./models/User");
 const bcrypt = require("bcryptjs");
+const Message = require("./models/Message");
+const ws = require("ws");
+const fs = require("fs");
+
 dotenv.config();
 const app = express();
 const CLIENT_URL = process.env.CLIENT_URL;
@@ -21,11 +25,6 @@ const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI);
 app.get("/", (req, res) => {
   res.send("<h1>This is a RESTful API for MERN Chat</h1>");
-});
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log("Server is running on http://localhost:" + PORT);
 });
 
 //User Register
@@ -85,4 +84,103 @@ app.get("/profile", (req, res) => {
   } else {
     res.status(401).json("No Token");
   }
+});
+
+//Run Server
+const PORT = process.env.PORT;
+const server = app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:" + PORT);
+});
+
+const wss = new ws.WebSocketServer({ server });
+
+wss.on("connection", (connection, req) => {
+  const notifyAboutOnlinePeople = () => {
+    [...wss.clients].forEach((client) => {
+      client.send(
+        JSON.stringify({
+          online: [...wss.clients].map((c) => ({
+            userId: c.userId,
+            username: c.username,
+          })),
+        })
+      );
+    });
+  };
+  connection.isAlive = true;
+
+  connection.timer = setInterval(() => {
+    connection.ping();
+    connection.deadTimer = setTimeout(() => {
+      connection.isAlive = false;
+      clearInterval(connection.timer);
+      connection.terminate();
+      notifyAboutOnlinePeople();
+      console.log("dead");
+    }, 1000);
+  }, 5000);
+  connection.on("pong", () => {
+    clearTimeout(connection.deadTimer);
+  });
+
+  //read username and id from cookie for this connection
+
+  const cookies = req.headers.cookie;
+  if (cookies) {
+    const tokenCookieString = cookies
+      .split(";")
+      .find((str) => str.startsWith("token="));
+    if (tokenCookieString) {
+      //token username woja
+      //token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzAwMDI0NzAwLCJleHAiOjE3MDAxMTExMDB9.rEFu-28Zy8mdINXzsvm8b-SF-r9FYl9vQ93fDorVFyk";
+      const token = tokenCookieString.split("=")[1];
+      if (token) {
+        jwt.verify(token, secret, {}, (err, userData) => {
+          if (err) throw err;
+          const { userId, username } = userData;
+          connection.userId = userId;
+          connection.username = username;
+        });
+      }
+    }
+  }
+
+  connection.on("message", async (message) => {
+    const messageData = JSON.parse(message.toString());
+    const { recipient, sender, text, file } = messageData;
+    let filename = null;
+    if (file) {
+      const parts = file.name.split(".");
+      const ext = parts[parts.length - 1];
+      filename = Date.now() + "." + ext;
+      const path = __dirname + "/uploads/" + filename;
+      const bufferData = new Buffer(file.data.split(",")[1], "base64");
+      fs.writeFile(path, bufferData, () => {
+        console.log("file saved: " + path);
+      });
+    }
+    if (recipient && (text || file)) {
+      const messageDoc = await Message.create({
+        sender: connection.userId,
+        recipient,
+        text,
+        file: file ? filename : null,
+      });
+      [...wss.clients]
+        .filter((c) => c.userId === recipient)
+        .forEach((c) =>
+          c.send(
+            JSON.stringify({
+              text,
+              file: file ? filename : null,
+              sender: connection.userId,
+              recipient,
+              _id: messageDoc._id,
+            })
+          )
+        );
+    }
+  });
+
+  notifyAboutOnlinePeople();
 });
